a20821b030fa42beb76c570521b35f40c6f5d8b4
MehmetSinanTatar <mstdeveloper@MehmetSinanTatar.local>
2024-12-26 09:01:08 +0300

    l_2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,16 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
+    id("dagger.hilt.android.plugin")
+
 }
 
 android {
     namespace = "com.mst.fooddelivery"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.mst.fooddelivery"
@@ -66,4 +71,50 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Livedata
+    implementation("androidx.compose.runtime:runtime-livedata:1.7.6")
+
+    // Material Icon
+    implementation("androidx.compose.material:material-icons-extended:1.7.6")
+    implementation("androidx.compose.material:material-icons-core:1.7.6")
+
+    // Retrofitt
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.9.0")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+
+    // Lifecycle
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+
+    // Dagger Hilt
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+    ksp("com.google.dagger:hilt-android-compiler:2.51.1")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    implementation("androidx.work:work-runtime:2.10.0")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-compose:2.8.5")
+
+    // Coil
+    implementation("io.coil-kt:coil-compose:2.3.0")
+
+    //Gson
+    implementation("com.google.code.gson:gson:2.10.1")
+    // WorkManager
+    implementation("androidx.work:work-runtime-ktx:2.10.0")
+    // splash screen
+    implementation(libs.core.splashscreen)
 }
---
+++ app/src/main/java/com/mst/fooddelivery/FootHupApp.kt
@@ -0,0 +1,8 @@
+package com.mst.fooddelivery
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FootHupApp : Application() {
+}
